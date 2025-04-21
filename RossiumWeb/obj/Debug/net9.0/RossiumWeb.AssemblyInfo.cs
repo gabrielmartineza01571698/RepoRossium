806a88451f0e7b128ea98900fb508632486d67c4
gabrielmartineza01571698 <a01571698@tec.mx>
2025-04-21 16:31:09 -0600

    gabriel

--- RossiumWeb/obj/Debug/net9.0/RossiumWeb.AssemblyInfo.cs
+++ RossiumWeb/obj/Debug/net9.0/RossiumWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RossiumWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2505a22f386858f0c2075f719e866c987bbd12e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63db76f2b32b9e63835fce0ae61bc78fda9c063")]
 [assembly: System.Reflection.AssemblyProductAttribute("RossiumWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RossiumWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
